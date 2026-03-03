8c28e4be2e9ff64a49ba4f83fa5ab3cce8253746
HanfuZhao781 <hz360@duke.edu>
2026-03-03 12:22:03 -0500

    Add Vercel and Render deployment configs

--- NLP product/frontend/src/api.js
+++ NLP product/frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE = '/api'
+const BASE = import.meta.env.VITE_API_URL ?? '/api'
 
 export const analyzeSingle = (text) =>
   axios.post(`${BASE}/analyze/single`, { text }).then(r => r.data)
